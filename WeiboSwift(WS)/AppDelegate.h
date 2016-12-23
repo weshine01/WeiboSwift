377a2d1722b8b429e4d42938b61c70c986cd2bfc
user <user@wangweixiang.local>
2016-12-23 18:18:44 +0800

    Initial Commit

---
+++ WeiboSwift(WS)/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeiboSwift(WS)
+//
+//  Created by user on 2016/12/23.
+//  Copyright © 2016年 王韦翔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
